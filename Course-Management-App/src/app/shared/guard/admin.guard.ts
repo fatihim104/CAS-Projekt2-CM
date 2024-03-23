c084ccaa9809ea7e3d73673e8c697f9cbf6183e5
fatihim104 <fatihimal@gmail.com>
2024-03-23 17:22:07 +0100

    admin authority is set according to the current user role.

--- Course-Management-App/src/app/shared/guard/admin.guard.ts
+++ Course-Management-App/src/app/shared/guard/admin.guard.ts
@@ -1,33 +1,34 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { map, Observable, tap } from "rxjs";
-import { environment } from "src/environments/environment";
 import { AuthService } from "../../auth/services/auth.service";
+import { User, UserRole } from "../user/user.model";
+import { UserService } from "../user/user.service";
 
 
 @Injectable({ providedIn: 'root' })
 export class AdminGuard implements CanActivate {
+    currentUser$: Observable<User | undefined>;
     constructor(
         private authService: AuthService,
-        private router: Router
-        ) {}
+        private router: Router,
+        private userService: UserService
+        ) {
+            this.currentUser$ = this.userService.getCurrentUser();
+        }
 
     canActivate(
         route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot): boolean | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        
-        return this.authService.userSubject.pipe(
-            map(user => {
-                return !!user && user.email == environment.adminEmail;
-            }),
-            tap(isAdmin => {
-                if(!isAdmin) {
-                    this.router.navigate(['/auth']);
-                }
-            })
-        );        
-
-
+          
+        return this.userService.getCurrentUser().pipe(
+        map(user => !!user && user?.role === UserRole.ADMIN), 
+        tap(isAdmin => {
+          if (!isAdmin) {
+            this.router.navigate(['/auth']); 
+          }
+        })
+      );
     }
 
 }
